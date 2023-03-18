45f9ddbef9c5844f0f88eabe9be6ece87627305a
Iaroslav <Iaroslav.aleksandrovich.beldin@gmail.com>
2023-03-18 18:39:52 +0400

    added toggle button

--- Homework 2.02/ViewController.swift
+++ Homework 2.02/ViewController.swift
@@ -7,15 +7,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     
     @IBOutlet var lightsViews: [UIView]!
+    @IBOutlet var lightsSwitchButton: UIButton!
+    
+    private var currentLight = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         lightsViews.forEach { $0.alpha = 0.3 }
-        
+        lightsSwitchButton.layer.cornerRadius = 12
     }
     
     override func viewDidLayoutSubviews() {
@@ -24,6 +27,18 @@ class ViewController: UIViewController {
         lightsViews.forEach {
             $0.layer.cornerRadius = min(lightsViews[0].bounds.width, lightsViews[0].bounds.height) / 2.0
         }
+    }
+    
+    @IBAction func lightsSwitchAction() {
+        
+        if currentLight == lightsViews.count {
+            currentLight = 0
+        }
+        
+        lightsSwitchButton.setTitle("NEXT", for: .normal)
+        lightsViews.forEach { $0.alpha = 0.3 }
+        lightsViews[currentLight].alpha = 1
+        currentLight += 1
     }
     
 }
